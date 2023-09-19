29f69ecfc27f904e172b6db1432c977584d94112
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-09-18 22:34:20 -0500

    cambio a dos a produccion

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,7 +8,7 @@ const db = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     dialect: 'postgres',
-    ...process.env.NODE_ENV === "porduction"
+    ...process.env.NODE_ENV === "production"
     ? { dialectOptions: { ssl: { required: true, rejectUnauthorized } } }
     : {},
 });
