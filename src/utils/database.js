08998fac332bec59fe004581a45e69a1da01904d
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-09-18 22:50:05 -0500

    cambio final

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,16 +1,16 @@
 import { Sequelize } from "sequelize";
-import 'dotenv/config';
+import "dotenv/config";
 
 const db = new Sequelize({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dialect: 'postgres',
-    ...process.env.NODE_ENV === "production"
-    ? { dialectOptions: { ssl: { required: true, rejectUnauthorized } } }
-    : {},
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: "postgres",
+  ...(process.env.NODE_ENV === "production"
+    ? { dialecOptions: { ssl: { required: true, rejectUnauthorized: false } } }
+    : {}),
 });
 
 export default db;
